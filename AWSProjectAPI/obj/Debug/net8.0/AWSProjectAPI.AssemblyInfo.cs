3266d05a8d8640f6c7a8f5a6a86d9d5a9befc569
Rashmal <rashmalat@gmail.com>
2024-06-16 19:43:58 +0530

    Changes

--- AWSProjectAPI/obj/Debug/net8.0/AWSProjectAPI.AssemblyInfo.cs
+++ AWSProjectAPI/obj/Debug/net8.0/AWSProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2dd1e56480fd0dc37c12ac214acca960dfc7567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bccbcb8971f0ef6b72b56218b0682ccd64357a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
